1f718561f13b2af2759271cdebc3c57fb737ddfe
Sergey Sova <mail@sergeysova.com>
2018-02-09 22:40:07 +0300

    feat: add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { configure, addDecorator } from '@storybook/react';
+
+function loadStories() {
+  const req = require.context('../client/', true, /\.story\.js$/)
+
+  req.keys().forEach(req)
+}
+
+
+const RootDecorator = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 30px;
+`
+
+addDecorator((fn) => (
+  <RootDecorator>{fn()}</RootDecorator>
+))
+
+configure(loadStories, module);
