b63a4e75e046fa8e348daa8e369768058d161203
Sergey Sova <mail@sergeysova.com>
2018-02-10 02:18:49 +0300

    feat(client/ui): add molecule Field

--- client/ui/theme.js
+++ client/ui/theme.js
@@ -5,8 +5,13 @@ export const color = {
   backgroundLight: '#fafafa',
 
   text: '#262626',
+  textLight: '#999',
 
   border: '#efefef',
+}
+
+export const font = {
+  formElement: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
 }
 
 export const globalStyles = `
