f933c83189c57da7937426ca3c975f3b37bfcfe6
Sergey Sova <mail@sergeysova.com>
2018-02-17 20:50:54 +0300

    refactor(client): to features

--- client/routes.js
+++ client/routes.js
@@ -1,73 +1,12 @@
 import React, { Fragment } from 'react'
-import styled from 'styled-components'
-import Helmet from 'react-helmet'
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
-import { CommonTemplate, Litter } from './ui'
+import { NotFountPage } from './features/not-found'
+import { FeedPage } from './features/feed'
+import { SettingsPage } from './features/settings'
+import { PhotoPage } from './features/photo'
+import { UserPage } from './features/user'
 
-/* eslint-disable react/prop-types */
-
-const CenterContainer = styled.div`
-  display: flex;
-  flex-flow: column;
-  flex-grow: 1;
-  align-items: center;
-`
-
-const NotFountPage = () => (
-  <CommonTemplate>
-    <Helmet title="Page Not Found" />
-    <CenterContainer>
-      <h2>Sorry, this page isn{'\''}t available.</h2>
-      <p>
-        The link you followed may be broken, or the page may have been removed.
-        {' '}
-        <Link to="/">Go back to Instaclone.</Link>
-      </p>
-    </CenterContainer>
-  </CommonTemplate>
-)
-
-const FeedPage = () => (
-  <CommonTemplate>
-    <Helmet title="Instaclone" titleTemplate="%s" />
-    Home page | <Link to="/settings">Settings</Link><br />
-    <Link to="/@sergeysova">@sergeysova</Link>
-  </CommonTemplate>
-)
-
-const SettingsPage = () => (
-  <CommonTemplate>
-    <Helmet title="Settings" />
-    Settings | <Link to="/">Feed</Link>
-  </CommonTemplate>
-)
-
-const Card = styled.div`
-  display: flex;
-  background-color: white;
-  border-radius: 4px;
-  padding: 1rem;
-`
-
-const ImagePage = ({ match, history }) => (
-  <Litter onClick={() => history.push(`/@${match.params.username}`)}>
-    <Card>
-      Image: {match.params.image} of {match.params.username}
-    </Card>
-  </Litter>
-)
-
-const UserPage = ({ match }) => (
-  <CommonTemplate>
-    <Helmet title={`@${match.params.username}`} />
-    User: {match.params.username} <br />
-    <Link to="/@sergeysova/i/1">#1</Link>
-    {/* <Switch>
-      <Route path="/@:username/i/:image" exact component={ImagePage} />
-    </Switch> */}
-  </CommonTemplate>
-)
 
 export const Routes = () => (
   <Fragment>
@@ -84,7 +23,7 @@ export const Routes = () => (
 
 export const PopupRoutes = () => (
   <Switch>
-    <Route path="/@:username/i/:image" exact component={ImagePage} />
+    <Route path="/@:username/i/:image" exact component={PhotoPage} />
   </Switch>
 )
 
