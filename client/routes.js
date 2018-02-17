a5fc003d066b57bac156b953c5d3114a16173a1c
Sergey Sova <mail@sergeysova.com>
2018-02-17 04:01:43 +0300

    refactor(client): use common template

--- client/routes.js
+++ client/routes.js
@@ -2,44 +2,29 @@ import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import { Route, Switch, Link } from 'react-router-dom'
 
+import { CommonTemplate, Litter } from './ui'
+
 /* eslint-disable react/prop-types */
 
-const GenericTemplate = styled.div`
-  padding: 1rem;
-`
-
 const NotFountPage = () => (
-  <GenericTemplate>
+  <CommonTemplate>
     <h1>404</h1>
     <h4>Page not found</h4>
-  </GenericTemplate>
+  </CommonTemplate>
 )
 
 const FeedPage = () => (
-  <GenericTemplate>
+  <CommonTemplate>
     Home page | <Link to="/settings">Settings</Link><br />
     <Link to="/@sergeysova">@sergeysova</Link>
-  </GenericTemplate>
+  </CommonTemplate>
 )
 
 const SettingsPage = () => (
-  <GenericTemplate>
+  <CommonTemplate>
     Settings | <Link to="/">Feed</Link>
-  </GenericTemplate>
+  </CommonTemplate>
 )
-
-const Platter = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, 0.7);
-`
 
 const Card = styled.div`
   display: flex;
@@ -49,21 +34,21 @@ const Card = styled.div`
 `
 
 const ImagePage = ({ match, history }) => (
-  <Platter onClick={() => history.push(`/@${match.params.username}`)}>
+  <Litter onClick={() => history.push(`/@${match.params.username}`)}>
     <Card>
       Image: {match.params.image} of {match.params.username}
     </Card>
-  </Platter>
+  </Litter>
 )
 
 const UserPage = ({ match }) => (
-  <GenericTemplate>
+  <CommonTemplate>
     User: {match.params.username} <br />
     <Link to="/@sergeysova/i/1">#1</Link>
     {/* <Switch>
       <Route path="/@:username/i/:image" exact component={ImagePage} />
     </Switch> */}
-  </GenericTemplate>
+  </CommonTemplate>
 )
 
 export const Routes = () => (
