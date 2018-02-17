dbdcb5a4862c7b95d2ca61520e05e31a05205ab1
Sergey Sova <mail@sergeysova.com>
2018-02-17 03:58:24 +0300

    docs(client/ui): add click events for Button

--- client/ui/atoms/button.story.js
+++ client/ui/atoms/button.story.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import { Button } from './button'
 
 
 storiesOf('atoms/Button', module)
   .add('Default', () => (
-    <Button>Default button</Button>
+    <Button onClick={action('default')}>Default button</Button>
   ))
   .add('Default wide', () => (
-    <Button wide>Wide button</Button>
+    <Button onClick={action('wide')} wide>Wide button</Button>
   ))
