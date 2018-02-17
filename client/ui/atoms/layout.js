2981d9cc0418dc21cfb0375d2ff900c77add5842
Sergey Sova <mail@sergeysova.com>
2018-02-17 03:33:08 +0300

    feat(client/ui): add justify and width to Layout

--- client/ui/atoms/layout.js
+++ client/ui/atoms/layout.js
@@ -12,6 +12,14 @@ export const Layout = styled.div`
   flex-wrap: ${(p) => p.wrap};
   padding: ${(p) => `${p.padding}rem`};
 
+  ${(p) => p.width && css`
+    width: ${p.width}rem
+  `};
+
+  ${(p) => p.justify && css`
+    justify-content: ${p.justify}
+  `};
+
   ${(p) => p.gap && css`
     & > * + * {
       margin-${marginDir}: ${p.gap}rem;
@@ -24,10 +32,14 @@ Layout.propTypes = {
   wrap: PropTypes.oneOf(['wrap', 'nowrap']),
   padding: PropTypes.number,
   gap: PropTypes.number,
+  justify: PropTypes.oneOf(['center', 'flex-start', 'flex-end', 'space-between', 'space-around']),
+  width: PropTypes.number,
 }
 
 Layout.defaultProps = {
   wrap: 'nowrap',
   padding: 0,
   gap: 0,
+  justify: undefined,
+  width: 0,
 }
