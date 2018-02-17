347ce869e14254c202d0cf6f78e3a88257dd5beb
Sergey Sova <mail@sergeysova.com>
2018-02-17 20:21:23 +0300

    feat(client/ui): add Footer to CommonTemplate

--- client/ui/templates/common.js
+++ client/ui/templates/common.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { Container } from '../atoms'
-import { Header } from '../molecules'
+import { Header, Footer } from '../molecules'
 
 
 const RootContainer = styled.section`
+  display: flex;
   min-height: 100vh;
   flex-flow: column nowrap;
   font-size: 1.6rem;
+  padding-top: 12rem;
 `
 
 const PageContainer = styled.main`
   display: flex;
   flex-grow: 1;
   justify-content: center;
-  margin-top: 12rem;
 `
 
 export const CommonTemplate = ({ children, header }) => (
@@ -27,6 +28,7 @@ export const CommonTemplate = ({ children, header }) => (
         {children}
       </Container>
     </PageContainer>
+    <Footer />
   </RootContainer>
 )
 
